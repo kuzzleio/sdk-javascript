19659c48c9f4e8e017d1d17ee3d65ab3a5b3907f
Anthony Sendra <asendra@kaliop.com>
2015-06-11 18:14:07 +0200

    write/search/get persistence

--- sdk.js
+++ sdk.js
@@ -15,7 +15,7 @@ function kuzzleSDK(socketUrl) {
     var requestId = Math.uuid();
 
     // subscribe to feedback and map to callback function when receive a message :
-    this.socket.once(requestId, function(result){
+    this.socket.once(requestId, function(result) {
       if (result.error) {
         console.error(result.error);
         return false;
@@ -41,26 +41,42 @@ function kuzzleSDK(socketUrl) {
    * @param {String} action
    * @param {Object} body
    * @param {Boolean} persist
+   * @param {Function} callback
    */
-  this.write = function(collection, action, body, persist) {
+  this.write = function(collection, action, body, persist, callback) {
+    var requestId = Math.uuid();
+
+    this.socket.once(requestId, function(result) {
+      if (result.error) {
+        console.error(result.error);
+        return false;
+      }
+
+      if (callback) {
+        callback(result);
+      }
+    });
+
     if (persist === undefined) {
       persist = false;
     }
 
-    var msg = { action: action,
+    this.socket.emit('write', {
+      requestId: requestId,
+      action: action,
       persist: persist,
       collection: collection,
-      body: body };
-    this.socket.emit('write', msg );
+      body: body
+    });
   };
 
   /**
-   * Read document from ES according to a filter
+   * Search document from ES according to a filter
    * @param {String} collection
    * @param {Object} filters
    * @param {Function} callback
    */
-  this.read = function(collection, filters, callback) {
+  this.search = function(collection, filters, callback) {
     var requestId = Math.uuid();
 
     this.socket.once(requestId, function(result) {
@@ -77,6 +93,26 @@ function kuzzleSDK(socketUrl) {
       action: 'search',
       collection: collection,
       body: filters
+    });
+  };
+
+  this.get = function (collection, id, callback) {
+    var requestId = Math.uuid();
+
+    this.socket.once(requestId, function(result) {
+      if (result.error) {
+        console.error(result.error);
+        return false;
+      }
+
+      callback(result);
+    });
+
+    this.socket.emit('read', {
+      requestId: requestId,
+      action: 'get',
+      collection: collection,
+      id: id
     });
   }
 }
