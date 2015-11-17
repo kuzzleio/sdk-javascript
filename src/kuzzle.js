4da5727a5ef141fc095e27af94a8d669226e5010
scottinet <scottinet@kaliop.com>
2015-11-17 15:50:28 +0100

    updated to work with kuzzle v0.3.1

--- src/kuzzle.js
+++ src/kuzzle.js
@@ -393,6 +393,7 @@ Kuzzle.prototype.query = function (collection, controller, action, query, option
     now = Date.now(),
     object = {
       action: action,
+      controller: controller,
       metadata: this.metadata
     },
     self = this;
@@ -440,7 +441,7 @@ Kuzzle.prototype.query = function (collection, controller, action, query, option
     });
   }
 
-  self.socket.emit(controller, object);
+  self.socket.emit('kuzzle', object);
 
   // Track requests made to allow KuzzleRoom.subscribeToSelf to work
   self.requestHistory[object.requestId] = now;
