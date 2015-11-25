5cbd9ea8c8671d4e20fb5d9d8b7bc9cce2e0f891
scottinet <scottinet@kaliop.com>
2015-11-25 10:31:46 +0100

    release v1.0 version

--- test/kuzzleDataCollection/methods.test.js
+++ test/kuzzleDataCollection/methods.test.js
@@ -6,5 +6,43 @@ var
   KuzzleDataCollection = rewire('../../src/kuzzleDataCollection');
 
 describe('KuzzleDataCollection methods', function () {
+  var
+    expectedQuery,
+    passedOptions,
+    error,
+    result,
+    collection,
+    queryStub = function (c, controller, action, query, options, cb) {
+      emitted = true;
+      should(c).be.exactly(collection);
+      should(controller).be.exactly(expectedQuery.controller);
+      should(action).be.exactly(expectedQuery.action);
 
+      if (passedOptions) {
+        should(options).match(passedOptions);
+      }
+
+      if (expectedQuery.body) {
+        should(query.body).match(expectedQuery.body);
+      } else {
+        should(Object.keys(query).length).be.exactly(0);
+      }
+
+      cb(error, result);
+    },
+    emitted,
+    kuzzle;
+
+  describe('#advancedSearch', function () {
+    beforeEach(function () {
+      kuzzle = new Kuzzle('foo');
+      kuzzle.query = queryStub;
+      expectedQuery = {
+        action: 'search',
+        controller: 'read'
+      };
+    });
+
+    //it('')
+  });
 });
