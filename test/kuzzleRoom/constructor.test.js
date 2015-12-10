1f3f734f9c4c31eb4a620787ae3dc050dd581144
scottinet <scottinet@kaliop.com>
2015-12-10 15:56:06 +0100

    defaulted KuzzleRoom.subscribeToSelf to true and cleaned up old test directives

--- test/kuzzleRoom/constructor.test.js
+++ test/kuzzleRoom/constructor.test.js
@@ -21,26 +21,29 @@ describe('KuzzleRoom constructor', function () {
   it('should handle provided arguments correctly', function () {
     var room = new KuzzleRoom(dataCollection);
 
-    should(room.listenToConnections).be.false();
-    should(room.listenToDisconnections).be.false();
     should(room.metadata).be.an.Object().and.be.empty();
-    should(room.subscribeToSelf).be.false();
+    should(room.subscribeToSelf).be.true();
+    should(room.scope).be.exactly('all');
+    should(room.state).be.exactly('done');
+    should(room.users).be.exactly('none');
     should(room.collection).be.exactly('foo');
     should(room.filters).be.null();
     should(room.headers).match({foo: 'bar'});
     should(room.roomId).be.null();
 
     room = new KuzzleRoom(dataCollection, {
-      listenToConnections: true,
-      listenToDisconnections: true,
+      scope: 'in',
+      state: 'pending',
+      users: 'all',
       metadata: {some: 'metadata'},
-      subscribeToSelf: true
+      subscribeToSelf: false
     });
 
-    should(room.listenToConnections).be.true();
-    should(room.listenToDisconnections).be.true();
     should(room.metadata).match({some: 'metadata'});
-    should(room.subscribeToSelf).be.true();
+    should(room.subscribeToSelf).be.false();
+    should(room.scope).be.exactly('in');
+    should(room.state).be.exactly('pending');
+    should(room.users).be.exactly('all');
   });
 
   it('should expose documented properties with the right permissions', function () {
@@ -49,16 +52,9 @@ describe('KuzzleRoom constructor', function () {
     should(room).have.propertyWithDescriptor('collection', {enumerable: true, writable: false, configurable: false});
     should(room).have.propertyWithDescriptor('filters', {enumerable: true, writable: true, configurable: false});
     should(room).have.propertyWithDescriptor('headers', {enumerable: true, writable: true, configurable: false});
-    should(room).have.propertyWithDescriptor('listenToConnections', {
-      enumerable: true,
-      writable: true,
-      configurable: false
-    });
-    should(room).have.propertyWithDescriptor('listenToDisconnections', {
-      enumerable: true,
-      writable: true,
-      configurable: false
-    });
+    should(room).have.propertyWithDescriptor('scope', {enumerable: false, writable: false, configurable: false});
+    should(room).have.propertyWithDescriptor('state', {enumerable: false, writable: false, configurable: false});
+    should(room).have.propertyWithDescriptor('users', {enumerable: false, writable: false, configurable: false});
     should(room).have.propertyWithDescriptor('metadata', {enumerable: true, writable: true, configurable: false});
     should(room).have.propertyWithDescriptor('subscribeToSelf', {
       enumerable: true,
