2fc775d0adb743af6b12b953c76effadc779bff7
scottinet <scottinet@kaliop.com>
2016-02-04 10:38:18 +0100

    skip npm install when deploying

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ then
   git config --global user.name "Travis CI"
   git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-build
   cd travis-build
-  npm install
+  #npm install
+  ln -s  ${TRAVIS_BUILD_DIR}/node_modules
   grunt
   git add dist/
   git commit -am "Travis CI - [ci skip] - automatic dist folder"
