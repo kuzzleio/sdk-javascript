361394cdb4043f71e2879fadc8affb8ca4b424f7
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-03-23 14:39:26 +0100

    nipticking

--- src/kuzzle.js
+++ src/kuzzle.js
@@ -321,15 +321,15 @@ Kuzzle.prototype.connect = function () {
   });
 
   self.socket.on('connect_error', function (error) {
-    var connexionError = new Error('Unable to connect to kuzzle server at "' + self.url + '"');
+    var connectionError = new Error('Unable to connect to kuzzle server at "' + self.url + '"');
 
     self.state = 'error';
     self.emitEvent('error');
 
-    connexionError.internal = error;
+    connectionError.internal = error;
 
     if (self.connectCB) {
-      self.connectCB(connexionError);
+      self.connectCB(connectionError);
     }
   });
 
