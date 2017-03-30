fe5746de4d53d61c3e4e76d9cde17d84b2474096
Anthony Sendra <asendra@kaliop.com>
2017-03-30 14:57:53 +0200

    add _kuzzle_info in Document

--- src/Document.js
+++ src/Document.js
@@ -18,9 +18,10 @@
  * @param {Collection} collection - an instanciated Collection object
  * @param {string} [documentId] - ID of an existing document
  * @param {object} [content] - Initializes this document with the provided content
+ * @param {object} [kuzzleInfo] - Initializes this document with the provided _kuzzle_info
  * @constructor
  */
-function Document(collection, documentId, content) {
+function Document(collection, documentId, content, kuzzleInfo) {
   Object.defineProperties(this, {
     // read-only properties
     collection: {
@@ -55,6 +56,11 @@ function Document(collection, documentId, content) {
       value: undefined,
       enumerable: true,
       writable: true
+    },
+    kuzzleInfo: {
+      value: kuzzleInfo || null,
+      enumerable: true,
+      writable: false
     }
   });
 
@@ -108,6 +114,7 @@ Document.prototype.serialize = function () {
   }
 
   data.body = this.content;
+  data.kuzzleInfo = this.kuzzleInfo;
   data._version = this.version;
   data = this.kuzzle.addHeaders(data, this.headers);
 
@@ -179,7 +186,7 @@ Document.prototype.refresh = function (options, cb) {
       return cb(error);
     }
 
-    newDocument = new Document(self.dataCollection, self.id, res.result._source);
+    newDocument = new Document(self.dataCollection, self.id, res.result._source, res.result._kuzzle_info);
     newDocument.version = res.result._version;
 
     cb(null, newDocument);
