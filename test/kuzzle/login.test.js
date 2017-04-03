ff164ff961dd86b3ab481bd21d38ffe94cee98c4
ballinette <gballini@kaliop.com>
2017-04-03 17:00:06 +0200

    cleanup unit tests for kuzzle login() and logout() methods

--- test/kuzzle/login.test.js
+++ test/kuzzle/login.test.js
@@ -5,190 +5,155 @@ var
 
 describe('Kuzzle Login', function () {
   var
-    kuzzle,
-    queryStub;
+    sandbox = sinon.sandbox.create(),
+    loginCredentials = {username: 'foo', password: 'bar'},
+    kuzzle;
 
   beforeEach(function() {
-    kuzzle = new Kuzzle('somewhere', {
-      connect: 'manual'
-    });
-    queryStub = sinon.stub(kuzzle, 'query');
+    kuzzle = new Kuzzle('somewhere', {connect: 'manual', eventTimeout: 0});
   });
+
   afterEach(function() {
-    queryStub.restore();
+    sandbox.restore();
   });
 
-  it('should have the token in login callback', function (done) {
-    var loginCredentials = {username: 'foo', password: 'bar'};
+  describe('# with callback', function () {
+    var queryStub;
 
-    this.timeout(200);
-
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(null, {result: {jwt: 'test-toto'}});
-    };
-
-    kuzzle.login('local', loginCredentials, '1h', function() {
-      should(kuzzle.jwtToken).be.exactly('test-toto');
-      done();
-    });
-  });
-
-  it('should handle login with only one argument and without callback', function () {
-    kuzzle.login('local');
-    should(queryStub).be.calledOnce();
-  });
-
-  it('should handle login with credentials and without callback', function () {
-    var loginCredentials = {username: 'foo', password: 'bar'};
-
-    kuzzle.login('local', loginCredentials);
-    should(queryStub).be.calledOnce();
-  });
-
-  it('should handle login without credentials, with expiresIn and without callback', function () {
-    kuzzle.login('local', '1h');
-    should(queryStub).be.calledOnce();
-  });
-
-  it('should handle login without credentials, with expiresIn and with callback', function (done) {
-    this.timeout(200);
-
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(null, {result: {jwt: 'test-toto'}});
-    };
-
-    kuzzle.login('local', '1h', function() {
-      done();
-    });
-  });
-
-  it('should handle login without credentials, without expiresIn and with callback', function (done) {
-    this.timeout(200);
-
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(null, {result: {jwt: 'test-toto'}});
-    };
-
-    kuzzle.login('local', function() {
-      done();
-    });
-  });
-
-  it('should handle login with credentials', function (done) {
-    var loginCredentials = {username: 'foo', password: 'bar'};
-
-    this.timeout(200);
-
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(null, {result: {jwt: 'test-toto'}});
-    };
-
-    kuzzle.login('local', loginCredentials, function() {
-      done();
-    });
-  });
-
-  it('should send a failed loginAttempt event if logging in fails', function (done) {
-    var eventEmitted = false;
-
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb({message: 'foobar'});
-    };
-
-    kuzzle.addListener('loginAttempt', function (status) {
-      should(status.success).be.false();
-      should(status.error).be.eql('foobar');
-      eventEmitted = true;
+    beforeEach(function() {
+      queryStub = sandbox.stub(kuzzle, 'query', function(queryArgs, query, options, cb) {
+        cb(null, {result: {jwt: 'test-toto'}});
+      });
     });
 
-    kuzzle.login('local', {});
-
-    setTimeout(function () {
-      should(eventEmitted).be.true();
-      done();
-    }, 0);
-  });
-
-  it('should not forward an event if there is no JWT token in the response', function (done) {
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(null, {result: {}});
-    };
-
-    kuzzle.addListener('loginAttempt', function () {
-      done('test failed');
-    });
-
-    kuzzle.login('local', {});
-
-    setTimeout(function () {
-      done();
-    }, 0);
-  });
-
-  it('should handle optional arguments correctly', function () {
-    var loginCredentials = {username: 'foo', password: 'bar'};
-
-    kuzzle.login('local', loginCredentials, function () {});
-    kuzzle.login('local', loginCredentials, '1h', function () {});
-    should(queryStub.firstCall).be.calledWithMatch(
-      {controller: 'auth', action: 'login'},
-      {body: {strategy: 'local', username: 'foo', password: 'bar'}}
-    );
-    should(queryStub.secondCall).be.calledWithMatch(
-      {controller: 'auth', action: 'login'},
-      {body: {strategy: 'local', username: 'foo', password: 'bar', expiresIn: '1h'}}
-    );
-  });
-
-  it('should have a empty token in logout callback', function () {
-    var unsetJwtToken = sinon.spy(kuzzle, 'unsetJwtToken');
-
-    kuzzle.logout();
-    should(unsetJwtToken).be.calledOnce();
-  });
-
-  it('should give an error if logout query fail to the logout callback if is set', function (done) {
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(new Error());
-    };
-
-    try {
-      kuzzle.logout(function(error) {
-        should(error).be.an.instanceOf(Error);
+    it('should handle login with only the strategy', function (done) {
+      kuzzle.login('local', function() {
+        should(queryStub).be.calledOnce();
         done();
       });
-    }
-    catch (err) {
-      done(err);
-    }
+    });
+
+    it('should handle login with credentials', function (done) {
+      kuzzle.login('local', loginCredentials, function() {
+        should(queryStub).be.calledOnce();
+        done();
+      });
+    });
+
+    it('should handle login without credentials and with expiresIn', function (done) {
+      kuzzle.login('local', '1h', function() {
+        should(queryStub).be.calledOnce();
+        done();
+      });
+    });
+
+    it('should have the token in login callback', function (done) {
+      kuzzle.login('local', loginCredentials, '1h', function() {
+        should(kuzzle.jwtToken).be.exactly('test-toto');
+        done();
+      });
+    });
   });
 
-  it('should give an error if login query fail to the login callback if is set', function (done) {
-    var loginCredentials = {username: 'foo', password: 'bar'};
+  describe('# without callback', function () {
+    var queryStub;
 
-    kuzzle.query = function(queryArgs, query, options, cb) {
-      cb(new Error());
-    };
+    beforeEach(function() {
+      queryStub = sandbox.stub(kuzzle, 'query');
+    });
 
-    try {
+    it('should handle login with only the strategy', function () {
+      kuzzle.login('local');
+      should(queryStub).be.calledOnce();
+    });
+
+    it('should handle login with credentials', function () {
+      kuzzle.login('local', loginCredentials);
+      should(queryStub).be.calledOnce();
+    });
+
+    it('should handle login without credentials and with expiresIn', function () {
+      kuzzle.login('local', '1h');
+      should(queryStub).be.calledOnce();
+    });
+
+    it('should handle optional arguments correctly', function () {
+      kuzzle.login('local', loginCredentials);
+      kuzzle.login('local', loginCredentials, '1h');
+      should(queryStub.firstCall).be.calledWith(
+        {controller: 'auth', action: 'login'},
+        {body: {strategy: 'local', username: 'foo', password: 'bar'}},
+        {queuable: false}
+      );
+      should(queryStub.secondCall).be.calledWith(
+        {controller: 'auth', action: 'login'},
+        {body: {strategy: 'local', username: 'foo', password: 'bar', expiresIn: '1h'}},
+        {queuable: false}
+      );
+    });
+  });
+
+
+  describe('#Error Login', function () {
+    it('should send a failed loginAttempt event if logging in fails', function (done) {
+      sandbox.stub(kuzzle, 'query', function(queryArgs, query, options, cb) {
+        cb({message: 'foobar'});
+      });
+
+      kuzzle.addListener('loginAttempt', function (status) {
+        should(status.success).be.false();
+        should(status.error).be.eql('foobar');
+        done();
+      });
+
+      kuzzle.login('local', {});
+    });
+
+    it('should not forward an event if there is no JWT token in the response', function (done) {
+      var loginAttemptStub = sinon.stub();
+      sandbox.stub(kuzzle, 'query', function(queryArgs, query, options, cb) {
+        cb(null, {result: {}});
+      });
+
+      kuzzle.addListener('loginAttempt', loginAttemptStub);
+
+      kuzzle.login('local', {});
+
+      process.nextTick(function () {
+        should(loginAttemptStub).not.be.called();
+        done();
+      });
+    });
+
+    it('should give an error if login query fail to the login callback if is set', function (done) {
+      sandbox.stub(kuzzle, 'query', function(queryArgs, query, options, cb) {
+        cb(new Error());
+      });
+
       kuzzle.login('local', loginCredentials, '1h', function(error) {
         should(error).be.an.instanceOf(Error);
         done();
       });
-    }
-    catch (err) {
-      done(err);
-    }
+    });
   });
 
-  it('should be able to send a login request', function () {
-    var loginCredentials = {username: 'foo', password: 'bar'};
+  describe('#Logout', function () {
+    it('should have a empty token in logout callback', function () {
+      var unsetJwtToken = sinon.spy(kuzzle, 'unsetJwtToken');
 
-    kuzzle.login('local', loginCredentials, '1h');
-    should(queryStub).be.calledWithMatch(
-      {controller: 'auth', action: 'login'},
-      {body: {strategy: 'local', username: 'foo', password: 'bar', expiresIn: '1h'}},
-      {queuable: false}
-    );
+      kuzzle.logout();
+      should(unsetJwtToken).be.calledOnce();
+    });
+
+    it('should give an error if logout query fail to the logout callback if is set', function (done) {
+      sandbox.stub(kuzzle, 'query', function(queryArgs, query, options, cb) {
+        cb(new Error());
+      });
+
+      kuzzle.logout(function(error) {
+        should(error).be.an.instanceOf(Error);
+        done();
+      });
+    });
   });
 });
