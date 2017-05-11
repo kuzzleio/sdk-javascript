69c5ead38a4264222d49245ffd74d05d5f3a1842
David Bengsch <dbengsch@kaliop.com>
2017-05-11 15:00:50 +0200

    Overload update method to take into account the new structure of Users

--- src/security/User.js
+++ src/security/User.js
@@ -27,7 +27,7 @@ function User(Security, id, content) {
     return Security.kuzzle.bluebird.promisifyAll(this, {
       suffix: 'Promise',
       filter: function (name, func, target, passes) {
-        var whitelist = ['create', 'replace', 'saveRestricted'];
+        var whitelist = ['create', 'replace', 'saveRestricted', 'update'];
 
         return passes && whitelist.indexOf(name) !== -1;
       }
@@ -183,6 +183,46 @@ User.prototype.serialize = function () {
  */
 User.prototype.getProfiles = function () {
   return this.content.profileIds;
+};
+
+/**
+ * Update the current KuzzleSecurityDocument into Kuzzle.
+ *
+ * @param {object} content - Content to add to KuzzleSecurityDocument
+ * @param {object} [options] - Optional parameters
+ * @param {responseCallback} [cb] - Handles the query response
+ * @returns {SecurityDocument} this
+ */
+User.prototype.update = function (content, options, cb) {
+  var
+    data = {},
+    self = this;
+
+  if (typeof content !== 'object') {
+    throw new Error('Parameter "content" must be a object');
+  }
+
+  if (options && cb === undefined && typeof options === 'function') {
+    cb = options;
+    options = null;
+  }
+
+  data._id = self.id;
+  data.body = content;
+
+  self.kuzzle.query(this.Security.buildQueryArgs(this.updateActionName), data, options, function (error, response) {
+    if (error) {
+      return cb ? cb(error) : false;
+    }
+
+    self.setContent({content: response.result._source, credentials: {}});
+
+    if (cb) {
+      cb(null, self);
+    }
+  });
+
+  return this;
 };
 
 module.exports = User;
