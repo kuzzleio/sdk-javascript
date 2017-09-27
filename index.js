2eb4b765bce3dd713d929b2a885d314f749024de
Luca Marchesini <luca@kuzzle.io>
2017-09-27 14:46:43 +0200

    Fixed typo.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (typeof window !== 'undefined' && typeof BUILT === 'undefined') {
   console.warn('It looks like you are using the Nodejs version of Kuzzle SDK ' +
                'in a browser. ' +
                'It is strongly recommended to use the browser-specific build instead. ' +
-               'Learn more at https://github.com/kuzzleio/sdk-javascript/tree/master#javascript');
+               'Learn more at https://github.com/kuzzleio/sdk-javascript/tree/master#browser');
 }
 
 module.exports = Kuzzle;
