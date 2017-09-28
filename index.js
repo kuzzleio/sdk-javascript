06095cd6b987b172be2234aea44f582f938f3663
Luca Marchesini <luca@kuzzle.io>
2017-09-28 11:08:07 +0200

    Replaced warning with Error

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var
 Kuzzle.prototype.bluebird = bluebird;
 
 if (typeof window !== 'undefined' && typeof BUILT === 'undefined') {
-  /* eslint-disable no-console */
-  console.warn('It looks like you are using the Nodejs version of Kuzzle SDK ' +
+  throw new Error('It looks like you are using the Nodejs version of Kuzzle SDK ' +
                'in a browser. ' +
                'It is strongly recommended to use the browser-specific build instead. ' +
                'Learn more at https://github.com/kuzzleio/sdk-javascript/tree/master#browser');
