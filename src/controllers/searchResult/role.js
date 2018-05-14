172bca460cf108e50dcb84b8acb8956eed176659
Ballinette <gballini@kaliop.com>
2018-05-14 02:31:47 +0200

    add unit tests for RoleSearchResult class

--- src/controllers/searchResult/role.js
+++ src/controllers/searchResult/role.js
@@ -8,17 +8,27 @@ class RoleSearchResult extends SearchResultBase {
     super(kuzzle, query, options, response);
 
     this.searchAction = 'searchRoles';
-    this.scrollAction = 'scrollRoles';
+    this.scrollAction = null; // scrollRoles action does not exists in Kuzzle API.
+
+    this.hits = this.response.hits.map(hit => new Role(this.kuzzle, hit._id, hit._source.controllers));
   }
 
   next () {
+    // in Kuzzle API, scrollRoles action is not available, and searchRoles allows only from and size parameters
+    // => we deny "scroll" and "sort" parameters.
+    if (this.request.scroll || this.request.sort) {
+      throw new Error('only from/size params are allowed for role search');
+    }
+
     return super.next()
       .then(result => {
         if (!result) {
           return result;
         }
 
-        return result.hits.map(hit => new Role(this.kuzzle, hit._id, hit._source.controllers));
+        this.hits = this.response.hits.map(hit => new Role(this.kuzzle, hit._id, hit._source.controllers));
+
+        return this;
       });
   }
 }
