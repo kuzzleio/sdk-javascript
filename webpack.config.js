77fd9d4575f2bdea4eba0f70d6ce18ba9d75a682
scottinet <seb.cottinet@gmail.com>
2019-01-09 11:14:46 +0100

    [webpack] make distinct packages for both node/react and browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,95 @@
 const
+  path = require('path'),
   webpack = require('webpack'),
   version = require('./package.json').version;
 
-module.exports = {
-  mode: 'production',
-  entry: './index.js',
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'kuzzle.js',
-    library: 'KuzzleSDK',
-    libraryTarget: 'umd'
-  },
-  target: 'node',
-  watch: false,
-  devtool: 'source-map',
-  node: {
-    console: false,
-    global: false,
-    process: false,
-    Buffer: false,
-    __filename: false,
-    __dirname: false,
-    setImmediate: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ['babel-loader', 'eslint-loader']
-      }
+module.exports = [
+  {
+    name: 'browser',
+    mode: 'production',
+    entry: './index.js',
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'kuzzle.js',
+      library: 'kuzzle-sdk',
+      libraryTarget: 'window'
+    },
+    target: 'web',
+    watch: false,
+    devtool: 'source-map',
+    node: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'babel.browsers.js')
+            }
+          }
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^(http|min-req-promise|package|ws)$/),
+      new webpack.DefinePlugin({
+        SDKVERSION: JSON.stringify(version),
+        BUILT: true
+      }),
+      new webpack.BannerPlugin('Kuzzle javascript SDK version ' + version),
+      new webpack.optimize.OccurrenceOrderPlugin()
     ]
   },
-  plugins: [
-    new webpack.IgnorePlugin(/^(http|min-req-promise|package|uws)$/),
-    new webpack.DefinePlugin({
-      SDKVERSION: JSON.stringify(version),
-      BUILT: true
-    }),
-    new webpack.BannerPlugin('Kuzzle javascript SDK version ' + version),
-    new webpack.optimize.OccurrenceOrderPlugin()
-  ]
-};
+  {
+    name: 'node/react',
+    mode: 'production',
+    entry: './index.js',
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'kuzzle.node.js',
+      library: 'KuzzleSDK',
+      libraryTarget: 'umd'
+    },
+    optimization: {
+      minimize: false
+    },
+    target: 'node',
+    watch: false,
+    devtool: 'source-map',
+    node: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'babel.node.js')
+            }
+          }
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^(bufferutil|utf-8-validate)$/),
+      new webpack.DefinePlugin({
+        SDKVERSION: JSON.stringify(version),
+        BUILT: true
+      }),
+      new webpack.BannerPlugin('Kuzzle javascript SDK version ' + version),
+      new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+  }
+];
