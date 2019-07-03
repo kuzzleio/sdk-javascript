a41ac8eb586afc3274d7f962ed2f76bf677aa66f
Berthier <berthier.est@gmail.com>
2019-07-03 09:06:15 +0200

    add package-lock

--- test-docs.sh
+++ test-docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 docker-compose -f .ci/doc/docker-compose.yml pull
 docker-compose -f .ci/doc/docker-compose.yml run doc-tests node index
 docker-compose -f .ci/doc/docker-compose.yml down
