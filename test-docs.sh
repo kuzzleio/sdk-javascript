ec85a14835ab91b8c7875bfdec3c4042ca859934
Berthier Esteban <44427849+berthieresteban@users.noreply.github.com>
2019-07-03 15:33:38 +0200

    Remove-vuejs-getting-started (#416)

--- test-docs.sh
+++ test-docs.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-set -e
 docker-compose -f .ci/doc/docker-compose.yml pull
 docker-compose -f .ci/doc/docker-compose.yml run doc-tests node index
+EXIT=$?
 docker-compose -f .ci/doc/docker-compose.yml down
 
-docker-compose -f .ci/doc/docker-compose.yml up -d kuzzle
-
-until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
-  printf '.'
-  sleep 5
-done
-
-cd doc/6/getting-started/vuejs
-npm ci
-npm run serve-without-vuex &
-npm run test
-
-cd ../../../../
-docker-compose -f .ci/doc/docker-compose.yml down
+exit $EXIT
