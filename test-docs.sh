d90b9cc76174bb928e0e8f9ca888c33726ab0232
Berthier <berthier.est@gmail.com>
2019-07-03 15:24:52 +0200

    test-doc script

--- test-docs.sh
+++ test-docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
 docker-compose -f .ci/doc/docker-compose.yml pull
 docker-compose -f .ci/doc/docker-compose.yml run doc-tests node index
+EXIT=$?
 docker-compose -f .ci/doc/docker-compose.yml down
+
+exit $EXIT
