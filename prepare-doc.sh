0726f5df99be3824ac107f0daa541e77f5e1c35b
Aschen <amaret93@gmail.com>
2019-07-19 11:52:34 +0200

    wip

--- prepare-doc.sh
+++ prepare-doc.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# mkdir -p doc-framework/src/sdk/js/
-
-# rm doc-framework/src/sdk/js/6
-
-ln -s ../../../../doc/6 doc-framework/src/sdk/js/6
-
 npm --prefix doc-framework/ install
-
-npm --prefix doc-framework/ run build
-
-ls doc-framework/src/.vuepress/dist
+./doc-framework/node_modules/.bin/vuepress build doc/6
