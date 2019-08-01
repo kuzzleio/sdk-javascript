c2136ac2cf7155c1fb6648eb5713aa281cd3609a
Berthier <Berthier.est@gmail.com>
2019-08-01 11:10:58 +0200

    add gs react

---
+++ doc/6/getting-started/.react/cypress/integration/chat.spec.js
@@ -0,0 +1,41 @@
+describe('test realtime chat', () => {
+
+  before(() => {
+    cy.initialisation();
+  });
+  
+  it('should fetch and display some messages', function () {
+    cy.fixture('Legolas.json').as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => {
+        cy.log(this.fixt);
+        cy.visit('/');
+        for (const message of this.fixt.messages) {
+          cy.contains(message.body.text);
+        }
+      });
+  });
+  
+  it('should send a message', function () {
+    cy.fixture('Sam.json').as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => {
+        cy.visit('/');
+        cy.get('#message')
+          .type(this.fixt.body.text);
+        cy.contains('Envoyer')
+          .click();
+        cy.contains(this.fixt.body.text);
+      });
+  });
+  
+  it('should receive a message', function () {
+    cy.fixture('Sauron.json').as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => {
+        cy.visit('/');
+        cy.createMessage(this.fixt.body);
+        cy.contains(this.fixt.body.text);
+      });
+  });
+});
