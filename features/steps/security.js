579e51b8c2ee24010639b99757b2bf5d9106e975
scottinet <seb.cottinet@gmail.com>
2019-09-12 11:28:38 +0200

    [compat] make this SDK compatible with node6 again

--- features/steps/security.js
+++ features/steps/security.js
@@ -1,47 +1,47 @@
 const {Given, When} = require('cucumber');
 
-Given('there is an user with id {string}', async function (id) {
+Given('there is an user with id {string}', function (id) {
   this.user = id;
 
-  try {
-    await this.kuzzle.security.createOrReplaceProfile('test', {
-      policies: [{roleId: 'admin'}]
-    });
-
-    this.content = await this.kuzzle.security.createUser(id, {
+  return this.kuzzle.security
+    .createOrReplaceProfile(
+      'test',
+      { policies: [{roleId: 'admin'}] })
+    .then(() => this.kuzzle.security.createUser(id, {
       content: {
         profileIds: ['test']
       },
       credentials: {}
+    }))
+    .then(content => {
+      this.content = content;
+    })
+    .catch(() => { /* do nothing */ });
+});
+
+Given('the user has {string} credentials with name {string} and password {string}', function (strategy, username, password) {
+  return this.kuzzle.security
+    .createCredentials(
+      strategy,
+      this.user,
+      { username, password })
+    .catch(() => this.kuzzle.security.updateCredentials(
+      strategy,
+      this.user,
+      { username, password }));
+});
+
+
+When('I get my user info', function () {
+  return this.kuzzle.security.getUser(this.user)
+    .then(content => {
+      this.content = content;
     });
-  }
-  catch (error) {
-    // do nothing
-  }
 });
 
-Given('the user has {string} credentials with name {string} and password {string}', async function (strategy, username, password) {
-  try {
-    await this.kuzzle.security.createCredentials(
-      strategy,
-      this.user,
-      { username, password });
-  }
-  catch (error) {
-    await this.kuzzle.security.updateCredentials(
-      strategy,
-      this.user,
-      { username, password });
-  }
-});
-
-
-When('I get my user info', async function () {
-  this.content = await this.kuzzle.security.getUser(this.user);
-});
-
-When('I update my user custom data with the pair {string}:{string}', async function (key, val) {
-  this.content = await this.kuzzle.security.updateUser(this.user, {
-    [key]: val
-  });
+When('I update my user custom data with the pair {string}:{string}', function (key, val) {
+  return this.kuzzle.security.updateUser(this.user, { [key]: val })
+    .then(content => {
+      this.content = content;
+    });
 });
