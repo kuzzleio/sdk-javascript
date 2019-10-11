9760f95a9aff62775ccf20c515af66952adc5644
Berthier Esteban <44427849+berthieresteban@users.noreply.github.com>
2019-10-11 15:06:27 +0200

    Add getting started Vue.js (#431)
    ## What does this PR do?
    
    This getting-started has been added to the documentation before but removed because vuepress can't build with it. 
    
    It was approved in those PR : #405 and #406 
    
    Add VueJS getting started without vuex to SDK JS6 documentation.
    Resolve the build problem of the vuepress doc (Vuepress tried to build .vue files of the getting-started and crash) by adding symlink.
    
    ![image](https://user-images.githubusercontent.com/44427849/62125816-7b1cc180-b2ce-11e9-83e2-df65d7dbcbd4.png)
    
    :warning:  need to add following line in doc/6/getting-started/vuejs/standalone/index.md line 14 after merge: 
    ``` You can find the full code of this guide [here](https://github.com/kuzzleio/sdk-javascript/tree/6-dev/doc/6/getting-started/vuejs/standalone).```:warning: 
    
    
    ### How should this be manually tested?
    See documentation: 
      - Step 1 : npm run doc prepare
      - Step 2 : npm run doc-dev
    
    Test getting started project (You'll need the CYPRESS_RECORD_KEY as environment variable)
      - Step 1 : Get a Kuzzle running without anonymous restrictions
      - Step 2 : cd doc/6/getting-started/vuejs && npm ci
      - Step 3 : npm run serve-standalone
      - Step 4 : npm run test
    
    ### Other changes
    /
    
    ### Boyscout
    /

--- test-docs.sh
+++ test-docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$here"
@@ -6,6 +7,21 @@ cd "$here"
 docker-compose -f .ci/doc/docker-compose.yml pull
 docker-compose -f .ci/doc/docker-compose.yml run doc-tests node index
 EXIT=$?
+docker-compose -f .ci/doc/docker-compose.yml down
+
+docker-compose -f .ci/doc/docker-compose.yml up -d kuzzle
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
+  printf '.'
+  sleep 5
+done
+
+cd doc/6/getting-started/.vuejs
+npm ci
+npm run serve-standalone &
+npm run test
+
+cd -
 docker-compose -f .ci/doc/docker-compose.yml down
 
 exit $EXIT
