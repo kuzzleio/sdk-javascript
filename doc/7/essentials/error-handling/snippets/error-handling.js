75720475852201f43ed03e950b37344cfd5aa458
Aschen <amaret93@gmail.com>
2019-10-14 10:27:23 +0200

    Fix functionnal test

--- doc/7/essentials/error-handling/snippets/error-handling.js
+++ doc/7/essentials/error-handling/snippets/error-handling.js
@@ -1,7 +1,7 @@
 kuzzle.index.create('nyc-open-data')
   .then(() => 'do something')
   .catch(error => {
-    if (error.status === 400) {
+    if (error.status === 412) {
       console.log(error.message);
       console.log('Try with another name!');
     }
