9c24dd2a3127dbca8f6d76298d3b5ade9665b1e7
Aschen <amaret93@gmail.com>
2019-10-15 10:42:25 +0200

    re-enable e2e tests

--- .ci/test-docs.sh
+++ .ci/test-docs.sh
@@ -5,29 +5,29 @@ set -e
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$here"
 
-# docker-compose -f ./doc/docker-compose.yml pull
-# docker-compose -f ./doc/docker-compose.yml run doc-tests node index
-# EXIT=$?
-# docker-compose -f ./doc/docker-compose.yml down
+docker-compose -f ./doc/docker-compose.yml pull
+docker-compose -f ./doc/docker-compose.yml run doc-tests node index
+EXIT=$?
+docker-compose -f ./doc/docker-compose.yml down
 
-# docker-compose -f ./doc/docker-compose.yml up -d kuzzle
+docker-compose -f ./doc/docker-compose.yml up -d kuzzle
 
-# until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
-#   printf '.'
-#   sleep 5
-# done
+until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
+  printf '.'
+  sleep 5
+done
 
 cd ${here}/../doc/7/getting-started/.react
 yarn install
 SKIP_PREFLIGHT_CHECK=true yarn start &
-# npm run test
+npm run test
 
-# cd ${here}/../doc/7/getting-started/.vuejs
-# npm ci
-# npm run serve-standalone &
-# npm run test
+cd ${here}/../doc/7/getting-started/.vuejs
+npm ci
+npm run serve-standalone &
+npm run test
 
-# cd ${here}
-# docker-compose -f ./doc/docker-compose.yml down
+cd ${here}
+docker-compose -f ./doc/docker-compose.yml down
 
-# exit $EXIT
+exit $EXIT
