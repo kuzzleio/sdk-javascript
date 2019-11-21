a1434246cb0870de2855aec8814254f9c62bb011
Aschen <amaret93@gmail.com>
2019-11-21 04:42:57 +0100

    add spinner

--- .ci/start_kuzzle.sh
+++ .ci/start_kuzzle.sh
@@ -2,11 +2,16 @@
 set -e
 # Download and launch custom Kuzzle stack
 
+echo "[$(date --rfc-3339 seconds)] - Start Kuzzle stack"
+
 docker-compose -f .ci/docker-compose.yml up -d
 
-printf 'Waiting for Kuzzle stack to be up and running'
-
+spinner="/"
 until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
-  printf '.'
-  sleep 5
+  printf '\r'
+  echo -n "[$(date --rfc-3339 seconds)] - Waiting for Kuzzle stack to be up and running [$spinner]"
+
+  if [ "$spinner" = "/" ]; then spinner="\\";  else spinner="/" ; fi
+
+  sleep 1
 done
