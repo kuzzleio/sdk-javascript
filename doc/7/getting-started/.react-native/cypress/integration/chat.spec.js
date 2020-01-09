14d65a6bdec78860d4e1715a36471fe07b9966b7
Berthier <Berthier.est@gmail.com>
2020-01-09 13:37:50 +0100

    clean cypress files

--- doc/7/getting-started/.react-native/cypress/integration/chat.spec.js
+++ doc/7/getting-started/.react-native/cypress/integration/chat.spec.js
@@ -1,83 +1,84 @@
 describe('test realtime chat', () => {
 
-    beforeEach(() => {
-        cy.viewport('iphone-6')
-    })
-    it('should enter a nickname', function () {
-      cy.fixture(`Elrond.json`).as('fixt')
-        .then(() => cy.loadEnvironment(this.fixt))
-        .then(() => cy.wait(2000))
-        .then(() => {
-          cy.visit('/');
-          cy.get('[placeholder="Send message..."]')
-            .should('not.exist');
-  
-          cy.get('[placeholder="Username"]')
-            .type(this.fixt.username)
-            .type('{enter}');
-  
-          cy.get('[placeholder="Send message..."]')
-            .should('exist');
-          cy.get('[placeholder="Username"]')
-            .should('not.exist');
-        });
-    });
-  
-    it('should fetch and display some messages', function () {
-      cy.fixture(`Legolas.json`).as('fixt')
-        .then(() => cy.loadEnvironment(this.fixt))
-        .then(() => cy.wait(2000))
-        .then(() => {
-          cy.visit('/');
-          cy.get('[placeholder="Username"]')
-            .type(this.fixt.username)
-            .type('{enter}');
-          for (const message of this.fixt.messages) {
-            cy.get(`[data-testid="${message.body.author === this.fixt.username ? 'fromMe' : 'fromOthers'}"]`)
-              .within(() => {
-                cy.contains(message.body.author);
-                cy.contains(message.body.message);
-              });
-          }
-        });
-    });
-  
-    it('should send a message', function () {
-      cy.fixture(`Sam.json`).as('fixt')
-        .then(() => cy.loadEnvironment(this.fixt))
-        .then(() => cy.wait(2000))
-        .then(() => {
-          cy.visit('/');
-          cy.get('[placeholder="Username"]')
-            .type(this.fixt.username)
-            .type('{enter}');
-          cy.get('[placeholder="Send message..."]')
-            .type(this.fixt.body.message)
-            .type('{enter}');
-          cy.get('[data-testid="fromMe"]')
-            .within(() => {
-              cy.contains(this.fixt.body.message);
-              cy.contains(this.fixt.body.author);
-            });
-        });
-    });
-  
-    it('should receive a message', function () {
-      cy.fixture(`Sauron.json`).as('fixt')
-        .then(() => cy.loadEnvironment(this.fixt))
-        .then(() => cy.wait(2000))
-        .then(() => {
-          cy.visit('/');
-          cy.get('[placeholder="Username"]')
-            .type(this.fixt.username)
-            .type('{enter}');
-          cy.wait(2000)
-          cy.createMessage(this.fixt.body);
-          cy.get('[data-testid="fromOthers"]')
-            .within(() => {
-              cy.contains(this.fixt.body.message);
-              cy.contains(this.fixt.body.author);
-            });
-        });
-    });
+  beforeEach(() => {
+    cy.viewport('iphone-6')
+  })
+
+  it('should enter a nickname', function () {
+    cy.fixture(`Elrond.json`).as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => cy.wait(2000))
+      .then(() => {
+        cy.visit('/');
+        cy.get('[placeholder="Send message..."]')
+          .should('not.exist');
+
+        cy.get('[placeholder="Username"]')
+          .type(this.fixt.username)
+          .type('{enter}');
+
+        cy.get('[placeholder="Send message..."]')
+          .should('exist');
+        cy.get('[placeholder="Username"]')
+          .should('not.exist');
+      });
   });
+
+  it('should fetch and display some messages', function () {
+    cy.fixture(`Legolas.json`).as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => cy.wait(2000))
+      .then(() => {
+        cy.visit('/');
+        cy.get('[placeholder="Username"]')
+          .type(this.fixt.username)
+          .type('{enter}');
+        for (const message of this.fixt.messages) {
+          cy.get(`[data-testid="${message.body.author === this.fixt.username ? 'fromMe' : 'fromOthers'}"]`)
+            .within(() => {
+              cy.contains(message.body.author);
+              cy.contains(message.body.message);
+            });
+        }
+      });
+  });
+
+  it('should send a message', function () {
+    cy.fixture(`Sam.json`).as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => cy.wait(2000))
+      .then(() => {
+        cy.visit('/');
+        cy.get('[placeholder="Username"]')
+          .type(this.fixt.username)
+          .type('{enter}');
+        cy.get('[placeholder="Send message..."]')
+          .type(this.fixt.body.message)
+          .type('{enter}');
+        cy.get('[data-testid="fromMe"]')
+          .within(() => {
+            cy.contains(this.fixt.body.message);
+            cy.contains(this.fixt.body.author);
+          });
+      });
+  });
+
+  it('should receive a message', function () {
+    cy.fixture(`Sauron.json`).as('fixt')
+      .then(() => cy.loadEnvironment(this.fixt))
+      .then(() => cy.wait(2000))
+      .then(() => {
+        cy.visit('/');
+        cy.get('[placeholder="Username"]')
+          .type(this.fixt.username)
+          .type('{enter}');
+        cy.wait(2000)
+        cy.createMessage(this.fixt.body);
+        cy.get('[data-testid="fromOthers"]')
+          .within(() => {
+            cy.contains(this.fixt.body.message);
+            cy.contains(this.fixt.body.author);
+          });
+      });
+  });
+});
--- doc/7/getting-started/.react-native/cypress/support/commands.js
+++ doc/7/getting-started/.react-native/cypress/support/commands.js
@@ -1,95 +1,82 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
 function reinitialisation() {
-    const kuzzle = Cypress.env('kuzzle');
-    // Clear collection
-    return cy
-      .request({
-        url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}`,
-        method: 'DELETE',
-        failOnStatusCode: false
-      })
-      .then(deleteResponse => {
-        cy.log(`Request : delete ${kuzzle.index} status : ${deleteResponse.status}`);
-        cy.wait(2000);
-      });
-  }
-  
-  Cypress.Commands.add('createMessage', (body) => {
-    const kuzzle = Cypress.env('kuzzle');
-    return cy
-      .request({
-        url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_create`,
-        method: 'POST',
-        body: body,
-      })
-      .its('body')
-      .then(response => {
-        cy.log(`Create status : ${response.status} {${body.author} / ${body.message}}`);
-        cy.wait(500);
-      });
-  });
-  
-  Cypress.Commands.add('initialisation', () => {
-    const kuzzle = Cypress.env('kuzzle');
-    // check if index exists
-    return cy
-      .request({
-        url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/_exists`,
-        method: 'GET'
-      })
-      .then(existsResponse => {
-        cy.wait(1000)
-        cy.log(`Request : exists ${kuzzle.index} status : ${existsResponse.status}`);
-        // Reset collection
-        if (existsResponse.result) {
-          return cy
+  const kuzzle = Cypress.env('kuzzle');
+  return cy
+    .request({
+      url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}`,
+      method: 'DELETE',
+      failOnStatusCode: false
+    })
+    .then(deleteResponse => {
+      cy.log(`Request : delete ${kuzzle.index} status : ${deleteResponse.status}`);
+      cy.wait(2000);
+    });
+}
+
+Cypress.Commands.add('createMessage', (body) => {
+  const kuzzle = Cypress.env('kuzzle');
+  return cy
+    .request({
+      url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_create`,
+      method: 'POST',
+      body: body,
+    })
+    .its('body')
+    .then(response => {
+      cy.log(`Create status : ${response.status} {${body.author} / ${body.message}}`);
+      cy.wait(500);
+    });
+});
+
+Cypress.Commands.add('initialisation', () => {
+  const kuzzle = Cypress.env('kuzzle');
+  // check if index exists
+  return cy
+    .request({
+      url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/_exists`,
+      method: 'GET'
+    })
+    .then(existsResponse => {
+      cy.wait(1000)
+      cy.log(`Request : exists ${kuzzle.index} status : ${existsResponse.status}`);
+      // Reset collection if exists
+      if (existsResponse.result) {
+        return cy
           .request({
             url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_truncate`,
             method: 'DELETE'
           })
-        }
-        return;
+      }
+      return;
+    });
+});
+
+Cypress.Commands.add('loadEnvironment', (env) => {
+  const kuzzle = Cypress.env('kuzzle');
+  if (!env.messages) {
+    return reinitialisation();
+  } else {
+    return cy
+      .initialisation()
+      .then(() => {
+        return cy
+          .request({
+            url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_mCreate`,
+            method: 'POST',
+            body: { "documents": env.messages },
+          })
+      })
+      .then(response => {
+        cy.log(`mCreate status : ${response.status}`);
+        cy.wait(500);
+        return cy
+          .request({
+            url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_refresh`,
+            method: 'POST',
+          })
+      })
+      .then((response) => {
+        cy.log(`refresh status : ${response.status}`);
+        cy.wait(500);
       });
-  });
-  
-  Cypress.Commands.add('loadEnvironment', (env) => {
-    const kuzzle = Cypress.env('kuzzle');
-    if (!env.messages) {
-      return reinitialisation();
-    } else {
-      return cy
-        .initialisation()
-        .then(() => {
-          return cy
-            .request({
-              url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_mCreate`,
-              method: 'POST',
-              body: { "documents": env.messages },
-            })
-        })
-        .then(response => {
-          cy.log(`mCreate status : ${response.status}`);
-          cy.wait(500);
-          return cy
-            .request({
-              url: `http://${kuzzle.host}:${kuzzle.port}/${kuzzle.index}/${kuzzle.collection}/_refresh`,
-              method: 'POST',
-            })
-        })
-        .then((response) => {
-          cy.log(`refresh status : ${response.status}`);
-          cy.wait(500);
-        });
-    }
-  });
+  }
+});
