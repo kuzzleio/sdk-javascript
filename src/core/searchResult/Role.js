3209ff1acbc186585d747325a9907f2bd95abf6c
Adrien Maret <amaret93@gmail.com>
2020-04-28 13:53:08 +0200

    Fix SearchResult.next with sort/size (#508)
    ## What does this PR do?
    
    :warning: Depends on https://github.com/kuzzleio/kuzzle/pull/1600
    
    Fix few bugs on the `SearchResult` pagination with `sort` and `size`.  
    
    The SDK now support every kind of ES sort:
     - string value
     - array of string
     - array of object
    
    Errors are now thrown when:
     - the sort is invalid
     - the sort combination does not identify one item only
    
    ### Other changes
    
     - the `next` method now return rejected promises instead of throwing (inconsistent behavior that can lead to unhandled rejection)
     - change `_uid` by `_id` because this field does not longer exists in ES 7.x

--- src/core/searchResult/Role.js
+++ src/core/searchResult/Role.js
@@ -16,7 +16,7 @@ class RoleSearchResult extends SearchResultBase {
     // in Kuzzle API, scrollRoles action is not available, and searchRoles allows only from and size parameters
     // => we deny "scroll" and "sort" parameters.
     if (this._request.scroll || this._request.sort) {
-      throw new Error('only from/size params are allowed for role search');
+      return Promise.reject(new Error('only from/size params are allowed for role search'));
     }
 
     return super.next()
--- src/core/searchResult/SearchResultBase.js
+++ src/core/searchResult/SearchResultBase.js
@@ -38,20 +38,42 @@ class SearchResultBase {
         .then(response => this._buildNextSearchResult(response));
     }
     else if (this._request.size && this._request.body.sort) {
-      const
-        request = Object.assign({}, this._request, {
-          action: this._searchAction
-        }),
-        hit = this._response.hits[this._response.hits.length - 1];
+      const request = { ...this._request, action: this._searchAction };
+      const hit = this._response.hits[this._response.hits.length - 1];
+
+      // When sorting only on a non unique field, the search_after will not iterate
+      // over all documents having the same values but ES will returns the results
+      // directly after.
+      // It resulting in having less fetched documents than the total and thus the SDK
+      // try to fetch the next results page but it's empty
+      if (! hit) {
+        return Promise.reject(new Error('Unable to retrieve all results from search: the sort combination must identify one item only. Add document "_id" to the sort.'));
+      }
 
       request.body.search_after = [];
 
-      for (const sort of this._request.body.sort) {
+      let sorts;
+      if (typeof this._request.body.sort === 'string') {
+        sorts = [this._request.body.sort];
+      }
+      else if (Array.isArray(this._request.body.sort)) {
+        sorts = this._request.body.sort;
+      }
+      else {
+        sorts = Object.keys(this._request.body.sort);
+      }
+
+      if (sorts.length === 0) {
+        return Promise.reject(new Error('Unable to retrieve next results from search: sort param is empty'));
+      }
+
+      for (const sort of sorts) {
         const key = typeof sort === 'string'
           ? sort
           : Object.keys(sort)[0];
-        const value = key === '_uid'
-          ? this._request.collection + '#' + hit._id
+
+        const value = key === '_id'
+          ? hit._id
           : this._get(hit._source, key.split('.'));
 
         request.body.search_after.push(value);
@@ -65,14 +87,15 @@ class SearchResultBase {
         return Promise.resolve(null);
       }
 
-      return this._kuzzle.query(Object.assign({}, this._request, {
+      return this._kuzzle.query({
+        ...this._request,
         action: this._searchAction,
         from: this.fetched
-      }), this._options)
+      }, this._options)
         .then(response => this._buildNextSearchResult(response));
     }
 
-    throw new Error('Unable to retrieve next results from search: missing scrollId, from/sort, or from/size params');
+    return Promise.reject(new Error('Unable to retrieve next results from search: missing scrollId, from/sort, or from/size params'));
   }
 
   _get (object, path) {
