3401e54d60c8174ab2309cd3980d5b9fa5c6cbb3
Aschen <adrien@maret.email>
2020-08-18 15:27:26 +0200

    I hate ci dev

--- doc/7/controllers/auth/search-api-keys/snippets/search-api-keys.js
+++ doc/7/controllers/auth/search-api-keys/snippets/search-api-keys.js
@@ -1,22 +1,22 @@
 try {
   const promises = [];
 
-  // Create some API keys for user "john.doe"
+  // Create some API keys for user "jared.doe"
   promises.push(
-    kuzzle.security.createApiKey('john.doe', 'Sigfox API key'));
+    kuzzle.security.createApiKey('jared.doe', 'Sigfox API key'));
   promises.push(
-    kuzzle.security.createApiKey('john.doe', 'LoRa 6 month API key', {
+    kuzzle.security.createApiKey('jared.doe', 'LoRa 6 month API key', {
       expiresIn: '6m'
     }));
   promises.push(
-    kuzzle.security.createApiKey('john.doe', 'LoRa permanent API key', {
+    kuzzle.security.createApiKey('jared.doe', 'LoRa permanent API key', {
       refresh: 'wait_for'
     }));
 
   await Promise.all(promises);
 
-  // Log as "john.doe"
-  await kuzzle.auth.login('local', { username: 'john.doe', password: 'password' });
+  // Log as "jared.doe"
+  await kuzzle.auth.login('local', { username: 'jared.doe', password: 'password' });
 
   const results = await kuzzle.auth.searchApiKeys({
     match: {
@@ -33,7 +33,7 @@ try {
         "_id": "znEwbG8BJASM_0-bWU-q",
         "_source": {
           "description": "LoRa permanent API key",
-          "userId": "john.doe",
+          "userId": "jared.doe",
           "expiresAt": -1,
           "ttl": -1
         }
@@ -42,7 +42,7 @@ try {
         "_id": "zXEwbG8BJASM_0-bWU-q",
         "_source": {
           "description": "LoRa 1 year API key",
-          "userId": "john.doe",
+          "userId": "jared.doe",
           "expiresAt": 31557600000,
           "ttl": 360000
         }
