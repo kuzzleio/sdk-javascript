757ed51b2e6f4e5b2c264179f67eb66a2f37145a
Aschen <adrien@maret.email>
2020-10-28 09:38:03 +0100

    fix api key

--- doc/7/controllers/auth/delete-api-key/snippets/delete-api-key.js
+++ doc/7/controllers/auth/delete-api-key/snippets/delete-api-key.js
@@ -1,7 +1,9 @@
 try {
   await kuzzle.auth.login('local', { username: 'jane.doe', password: 'password' });
 
-  await kuzzle.auth.deleteApiKey('fQRa28BsidO6V_wmOcL');
+  const apiKey = await kuzzle.auth.createApiKey('Sigfox API key', { refresh: 'wait_for '});
+
+  await kuzzle.auth.deleteApiKey(apiKey._id);
 
   console.log('API key successfully deleted');
 } catch (e) {
