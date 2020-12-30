04fdd676fcbb61550b925bf905bf195ac1985b1c
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2020-12-30 14:34:19 +0100

    Update src/controllers/Document.ts
    Co-authored-by: Adrien Maret <amaret93@gmail.com>

--- src/controllers/Document.ts
+++ src/controllers/Document.ts
@@ -781,7 +781,7 @@ export class DocumentController extends BaseController {
    * @param index Index name
    * @param collection Collection name
    * @param _id Unique document identifier
-   * @param body Partial changes to apply to the document and Fields to add to the document if it gets created (optional)
+   * @param changes Partial content of the document to update
    * @param options
    *    - `defaults` Fields to add to the document if it gets created
    *    - `refresh` If set to `wait_for`, Kuzzle will not respond until the API key is indexed
