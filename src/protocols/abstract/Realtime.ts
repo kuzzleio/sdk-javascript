01ab90e689d253bd9b563f9515ff85fb6d96e354
Yoann Abbes <yoann1.abbes@epitech.eu>
2021-01-14 16:10:58 +0100

    default value pingInterval 30s

--- src/protocols/abstract/Realtime.ts
+++ src/protocols/abstract/Realtime.ts
@@ -16,7 +16,7 @@ export abstract class BaseProtocolRealtime extends KuzzleAbstractProtocol {
 
     this._autoReconnect = typeof options.autoReconnect === 'boolean' ? options.autoReconnect : true;
     this._reconnectionDelay = typeof options.reconnectionDelay === 'number' ? options.reconnectionDelay : 1000;
-    this._pingInterval = typeof options.pingInterval === 'number' ? options.pingInterval : 2000;
+    this._pingInterval = typeof options.pingInterval === 'number' ? options.pingInterval : 30000;
 
     this.wasConnected = false;
     this.stopRetryingToConnect = false;
