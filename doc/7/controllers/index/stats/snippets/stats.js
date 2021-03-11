48838aae4e4d72e1d78c38af1bca22c49cbe7400
Michele <bluedraggy@gmail.com>
2021-03-11 13:58:34 +0100

    doc(index): stats result in snippet as JSON instead

--- doc/7/controllers/index/stats/snippets/stats.js
+++ doc/7/controllers/index/stats/snippets/stats.js
@@ -2,18 +2,20 @@ const stats = await kuzzle.index.stats();
 
 console.log(JSON.stringify(stats));
 /*
-  indexes: [
-    {
-      name: 'nyc-open-data',
-      size: 42,
-      collections: [
-        {
-          name: 'yellow-taxi',
-          documentCount: 42,
-          size: 42,
-        }
-      ]
-    }
-  ],
-  size: 42
+  {
+    "indexes":[
+      {
+        "name":"nyc-open-data",
+        "size":42,
+        "collections":[
+          {
+            "name":"yellow-taxi",
+            "documentCount":42,
+            "size":42
+          }
+        ]
+      }
+    ],
+    "size":42
+  }
 */
