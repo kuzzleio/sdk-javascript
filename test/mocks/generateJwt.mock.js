8d6d2c483a2e30a7aa9dabc508baf05a61b7cb6d
Robin GRANDVAL <robin@hallinta.fr>
2021-07-07 10:20:33 +0200

    [Test] Jwt expiration test : convert micro timestamp to timestamp

--- test/mocks/generateJwt.mock.js
+++ test/mocks/generateJwt.mock.js
@@ -1,10 +1,10 @@
 function generateJwt (userId = 'test-user', expiresAt = null) {
   const 
     header = { alg: 'HS256', typ: 'JWT' },
-    payload = { _id: userId, iat: Date.now(), exp: expiresAt },
+    payload = { _id: userId, iat: Math.round(Date.now() / 1000), exp: expiresAt },
     signature = 'who cares?';
 
-  expiresAt = expiresAt || Date.now() + 3600 * 1000;
+  expiresAt = expiresAt || Math.round(Date.now() / 1000) + 3600 * 1000;
 
   return [ header, payload, signature]
     .map(data => Buffer.from(JSON.stringify(data)).toString('base64'))
