95900ebfe986591c34d2859e8e70342eec8ebbf4
Shiranuit <shiranui007@gmail.com>
2021-08-02 17:37:18 +0200

    revert changes on ci script

--- .ci/test-docs.sh
+++ .ci/test-docs.sh
@@ -6,35 +6,35 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$here"
 
 docker-compose -f ./doc/docker-compose.yml pull
-# docker-compose -f ./doc/docker-compose.yml run doc-tests node index
-# EXIT=$?
-# docker-compose -f ./doc/docker-compose.yml down
+docker-compose -f ./doc/docker-compose.yml run doc-tests node index
+EXIT=$?
+docker-compose -f ./doc/docker-compose.yml down
 
 docker-compose -f ./doc/docker-compose.yml up -d kuzzle
 
-# until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
-#   printf '.'
-#   sleep 5
-# done
+until $(curl --output /dev/null --silent --head --fail http://localhost:7512); do
+  printf '.'
+  sleep 5
+done
 
-# cd ${here}/../doc/7/getting-started/.react/with-redux
-# npm ci
-# SKIP_PREFLIGHT_CHECK=true npm run start &
-# until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
-#   printf '.'
-#   sleep 5
-# done
-# npm run test
-# kill $(lsof -t -i:3000)
+cd ${here}/../doc/7/getting-started/.react/with-redux
+npm ci
+SKIP_PREFLIGHT_CHECK=true npm run start &
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+  printf '.'
+  sleep 5
+done
+npm run test
+kill $(lsof -t -i:3000)
 
-# cd ${here}/../doc/7/getting-started/.react/standalone
-# npm ci
-# SKIP_PREFLIGHT_CHECK=true npm run start &
-# until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
-#   printf '.'
-#   sleep 5
-# done
-# npm run test
+cd ${here}/../doc/7/getting-started/.react/standalone
+npm ci
+SKIP_PREFLIGHT_CHECK=true npm run start &
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+  printf '.'
+  sleep 5
+done
+npm run test
 
 cd ${here}/../doc/7/getting-started/.vuejs
 npm ci
@@ -45,15 +45,15 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8080); d
 done
 npm run test
 
-# cd ${here}/../doc/7/getting-started/.react-native
-# # Here we use install instead of ci because expo-cli cannot be installed with ci
-# npm install
-# npm run web &
-# until $(curl --output /dev/null --silent --head --fail http://localhost:19006); do
-#   printf '.'
-#   sleep 5
-# done
-# npm run test
+cd ${here}/../doc/7/getting-started/.react-native
+# Here we use install instead of ci because expo-cli cannot be installed with ci
+npm install
+npm run web &
+until $(curl --output /dev/null --silent --head --fail http://localhost:19006); do
+  printf '.'
+  sleep 5
+done
+npm run test
 
 cd ${here}
 docker-compose -f ./doc/docker-compose.yml down
