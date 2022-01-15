b47b8a908a9989927afe6e0bfd0120564b3eef54
Aschen <adrien@maret.email>
2022-01-15 21:32:32 +0100

    fix deepcompare

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,7 +1,36 @@
 import { JSONObject } from '../types';
 
 export function deepCompare (a: JSONObject, b: JSONObject): boolean {
-  return Object.entries(a).sort().toString() === Object.entries(b).sort().toString();
+  const entriesA = Object.entries(a).sort();
+  const entriesB = Object.entries(b).sort();
+
+  if (entriesA.length !== entriesB.length) {
+    return false;
+  }
+
+  for (let i = 0; i < entriesA.length; i++) {
+    const [kA, vA] = entriesA[i];
+    const [kB, vB] = entriesB[i];
+
+    if (kA !== kB) {
+      return false;
+    }
+
+    if (typeof vA !== typeof vA) {
+      return false;
+    }
+
+    if (typeof vA === 'object') {
+      if (! deepCompare(vA, vB)) {
+        return false;
+      }
+    }
+    else if (vA !== vB) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 export function omit (object: JSONObject, properties: string[]): JSONObject {
