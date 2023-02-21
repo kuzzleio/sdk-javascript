3eab20bec6926209ba5eb627f5fbbad9a34da117
Andréas HANSS <contact@codingspark.io>
2023-02-21 18:29:06 +0100

    Set JSONObject to proper type

--- src/types/JSONObject.ts
+++ src/types/JSONObject.ts
@@ -1,6 +1,4 @@
 /**
  * An interface representing an object with string key and any value
  */
-export interface JSONObject {
-  [key: string]: JSONObject | any;
-}
+export type JSONObject = Record<PropertyKey, any>;
