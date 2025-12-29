2b0369d0f006e7ef5ab7460cdd280f9c00aa76f2
rolljee <n.riquelmebareiro@gmail.com>
2025-12-29 21:04:45 +0100

    fix: export files that are used by other libs

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 export * from "./src/Kuzzle";
 export * from "./src/protocols";
 export * from "./src/protocols/abstract/Base";
-export * from "./src/core/KuzzleEventEmitter";
 
-export * from "./src/core/searchResult/SearchResultBase";
+export * from "./src/core/batchWriter/BatchController";
+export * from "./src/core/KuzzleEventEmitter";
+export * from "./src/core/Observer";
+export * from "./src/core/RealtimeDocument";
 export * from "./src/core/searchResult/Document";
 export * from "./src/core/searchResult/Profile";
 export * from "./src/core/searchResult/Role";
+export * from "./src/core/searchResult/SearchResultBase";
 export * from "./src/core/searchResult/Specifications";
 export * from "./src/core/searchResult/User";
-export * from "./src/core/Observer";
-export * from "./src/core/RealtimeDocument";
-export * from "./src/core/batchWriter/BatchController";
+export * from "./src/core/security/Profile";
+export * from "./src/core/security/Role";
+export * from "./src/core/security/User";
 
 export * from "./src/types";
 
